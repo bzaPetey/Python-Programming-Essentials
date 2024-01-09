8c552df8c40b89957db5be7445366ee93da0d8e0
bzaPetey <peter.laliberte@burgzergarcade.net>
2024-01-09 10:17:36 -0500

    updated jail time.

--- Bonus/prisonersDilemma.py
+++ Bonus/prisonersDilemma.py
@@ -1,46 +1,46 @@
 import random
 
 rounds = 10                 # number of rounds
-options = ['S', 'Q']        # options - snitch or quiet
+options = ['T', 'S']        # options - testify or stay silent
 jail_time = [0, 0]          # years in jail
 
 
 # calc jail time
 def calc_jail_time(player, computer):
     if player and computer:
-        jail_time[0] += 1
-        jail_time[1] += 1
-        print("Both players cooperate. Each gets 2 years in prison.")
+        jail_time[0] += 2
+        jail_time[1] += 2
+        print("Both players testify. Each gets 2 years in prison.")
 
     elif player and not computer:
         jail_time[1] += 3
-        print("The Player 1 betrays the Computer. Player 1 goes free, the Computer gets 5 years in prison.")
+        print("The Player 1 testifies the Computer. Player 1 goes free, the Computer gets 3 years in prison.")
 
     elif not player and computer:
         jail_time[0] += 3
-        print("The Computer betrays Player 1. The Computer goes free, Player 1 gets 5 years in prison.")
+        print("The Computer betrays Player 1. The Computer goes free, Player 1 gets 3 years in prison.")
 
     else:
-        jail_time[0] += 2
-        jail_time[1] += 2
-        print("Both players betray each other. Each gets 4 years in prison.")
+        jail_time[0] += 1
+        jail_time[1] += 1
+        print("Both players stay silent. Each gets 2 years in prison.")
 
 
 # Main code
 print("Prisoner's Dilemma Game")
-print("Enter 'S' to Snitch or 'Q' to Remain Quiet.")
+print("Enter 'T' to testify or 'S' to Remain Silent.")
 
 
 while rounds > 0:
-    player = input("Choose (S/Q): ").upper()
+    player = input("Choose (T/S): ").upper()
     computer = random.choice(options)
 
-    if player == 'S':
+    if player == 'T':
         player_cooperate = True
     else:
         player_cooperate = False
 
-    if computer == 'S':
+    if computer == 'T':
         computer_cooperate = True
     else:
         computer_cooperate = False
@@ -54,3 +54,4 @@ print(f'The Computer get {jail_time[1]} years in jail.')
 
 # who serves more time and by how much?
 # keep track of the picks for each round
+# what if tehy enter a different input
